e94f777368af37ea8005b6954796d740ba826213
Andrew Smith <andrew@andrew.codes>
2018-04-29 12:43:13 -0400

    Add Google analytics enabled routes for home and 404

--- website/src/containers/App.js
+++ website/src/containers/App.js
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled, { injectGlobal } from 'styled-components';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { withRouter } from 'react-router';
 
-export default () => (
-  <div>
-    <h1>Verdigris Heading</h1>
-  </div>
-);
+import Home from '../pages/Home';
+import GoogleAnalyticsListener from '../components/GoogleAnalyticsListener';
+import PageNotFound from '../pages/PageNotFound';
+import { GOOGLE_ANALYTICS_ID } from '../constants';
+
+const SiteAnalytics = ({ children }) => {
+  return (
+    <GoogleAnalyticsListener gaId={GOOGLE_ANALYTICS_ID}>
+      {children}
+    </GoogleAnalyticsListener>
+  );
+};
+
+class ScrollToTop extends Component {
+  componentDidUpdate(prevProps) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+    return null;
+  }
+}
+
+const ScrollHandler = withRouter(ScrollToTop);
+
+class RouteBoundary extends Component {
+  state = { hasError: false };
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    let { hasError } = this.state;
+    if (hasError) {
+      return <PageNotFound />;
+    }
+    return this.props.children;
+  }
+}
+
+const AppContent = styled.div`
+  flex: 1 1 auto;
+`;
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <SiteAnalytics>
+        <Route>
+          <ScrollHandler />
+        </Route>
+        <Switch>
+          <Route>
+            <RouteBoundary>
+              <AppContent>
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route path="/error" component={PageNotFound} />
+                  <Route component={PageNotFound} />
+                </Switch>
+              </AppContent>
+            </RouteBoundary>
+          </Route>
+        </Switch>
+      </SiteAnalytics>
+    </BrowserRouter>
+  );
+}
---
+++ website/src/pages/PageNotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>Oops, we couldn't find that page.</h1>
+    </div>
+  );
+}
