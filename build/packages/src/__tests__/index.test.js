31a54968e8b64f220536047ac5e5a520507c2ff4
Andrew Smith <andrew@andrew.codes>
2018-04-30 12:47:45 -0400

    Testing travis

--- build/packages/src/__tests__/index.test.js
+++ build/packages/src/__tests__/index.test.js
@@ -9,8 +9,7 @@ const arrayOfPackages = [
     location: 'path/to/package1',
   }, {
     location: 'path/to/package2',
-  }
-
+  },
 ];
 
 test('getting all packages returns all lerna packages from the current working directory', () => {
