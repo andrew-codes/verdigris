9716edb12217c3e679e676cb723c6c3f7f3f9e5c
Andrew Smith <andrew@andrew.codes>
2018-05-04 09:11:37 -0400

    Apply consistent page formatting to other pages

--- website/src/pages/Home.js
+++ website/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Page from '../components/Page';
 
 export default function Home() {
   return (
-    <div>
+    <Page>
       <h1>Verdigris Heading</h1>
       <Link to="/error">Error page</Link>
-    </div>
+    </Page>
   );
 }
--- website/src/pages/PageNotFound.js
+++ website/src/pages/PageNotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Page from '../components/Page';
 
 export default function PageNotFound() {
   return (
-    <div>
+    <Page>
       <h1>Oops, we couldn't find that page.</h1>
-    </div>
+    </Page>
   );
 }
