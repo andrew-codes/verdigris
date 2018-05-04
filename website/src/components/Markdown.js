058e5d582f76d2d316a8a5cfc2ab6c8ce85d2e79
Andrew Smith <andrew@andrew.codes>
2018-05-04 14:22:34 -0400

    Markdown handles both internal and external links

--- website/src/components/Markdown.js
+++ website/src/components/Markdown.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as PropTypes from 'prop-types';
 
 export default function Markdown({ children }) {
   return React.createElement(children, {
     components: {
-      code: (props) => <div>{props.children}</div>
+      code: props => <div>{props.children}</div>,
+      a: props => {
+        if (/http(s)?:\/\//.exec(props.href)) {
+          return <a {...props}>{props.children}</a>;
+        }
+        return <Link to={props.href} {...props} />;
+      },
     },
   });
 }
