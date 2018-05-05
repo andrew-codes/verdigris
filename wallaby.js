16076491b03202bf44efb2bdbcd42d6839118bed
Andrew Smith <andrew@andrew.codes>
2018-05-04 20:23:02 -0400

    Ignore node_modules and dist when running wallaby

--- wallaby.js
+++ wallaby.js
@@ -3,22 +3,31 @@ process.env.NODE_ENV = 'test';
 module.exports = wallaby => ({
   files: [
     'jest.config.js',
-    'build/**/*.js', '!build/**/__tests__/*.js',
-    'packages/**/*.js', '!packages/**/__tests__/*.js',
-    'website/**/*.js', '!website/**/__tests__/*.js',
+    'build/**/*.js',
+    '!build/**/__tests__/*.js',
+    'packages/**/*.js',
+    '!packages/**/__tests__/*.js',
+    'website/**/*.js',
+    '!website/**/__tests__/*.js',
+    '!node_modules/**/*.*',
+    '!website/dist',
+    '!**/node_modules/**/*.js',
   ],
   tests: [
     'build/**/__tests__/*.js',
     'packages/**/__tests__/*.js',
     'website/**/__tests__/*.js',
+    '!website/dist',
+    '!**/node_modules/**/*.js',
   ],
+  filesWithNoCoverageCalculated: ['package.json', 'jest.config.js'],
   env: {
     type: 'node',
     runner: 'node',
   },
   testFramework: 'jest',
   compilers: {
-    '**/*.js': wallaby.compilers.babel()
+    '**/*.js': wallaby.compilers.babel(),
   },
   setup(w) {
     const jestConfig = require('./jest.config.js');
