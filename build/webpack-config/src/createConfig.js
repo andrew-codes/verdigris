011a61d91fce2e605add663ffc612c74a2f75574
Andrew Smith <andrew@andrew.codes>
2018-05-23 10:32:38 -0400

    Netlify _redirects is handled by webpack

--- build/webpack-config/src/createConfig.js
+++ build/webpack-config/src/createConfig.js
@@ -117,6 +117,7 @@ function plugins(
     }),
     new CopyWebpackPlugin([
       { from: path.join(process.cwd(), '..', 'docs', 'assets/**/*'), to: path.join(process.cwd(), 'dist', 'assets'), flatten: true, },
+      { from: path.join(process.cwd(), '_redirects'), to: path.join(process.cwd(), 'dist'), type: 'file', },
     ], {
         debug: env === 'development',
       }),
