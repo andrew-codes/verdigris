6b22ca7251b0310bf88e7b2823040effda878ce1
Andrew Smith <andrew@andrew.codes>
2018-05-25 17:12:33 -0400

    Enable linking to internal package docs on package doc pages

--- build/docs/src/index.js
+++ build/docs/src/index.js
@@ -1,11 +1,21 @@
-import { CodeBlock, InlineCode } from '@verdigris/code';
 import markings from 'react-markings';
 import React from 'react';
+import { CodeBlock, InlineCode } from '@verdigris/code';
+import { withRouter } from 'react-router-dom';
+
+const Link = withRouter(({
+  children,
+  href,
+  location: {
+    pathname,
+  },
+}) => <a href={`${pathname}/${href}`} title="here">{children}</a>);
 
 export const md = markings.customize({
   renderers: {
+    code: props => <InlineCode>{props.literal}</InlineCode>,
     codeBlock: props => <CodeBlock language={props.language}>{props.literal}</CodeBlock>,
-    code: props => <InlineCode>{props.literal}</InlineCode>
+    link: Link,
   },
 });
 export { default as code } from './code';
