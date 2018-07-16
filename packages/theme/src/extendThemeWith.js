4459422c50f25b9a47d3892aabfeb96511f7ee28
Andrew Smith <andrew@andrew.codes>
2018-07-16 09:27:22 -0400

    chore(theme): co-locate theme with components

--- packages/theme/src/extendThemeWith.js
+++ packages/theme/src/extendThemeWith.js
@@ -1,15 +1,10 @@
 
 import { merge } from 'lodash';
 import { css } from 'emotion';
-
-const components = [
-  'Chip',
-  'Icon',
-  'Switch',
-];
+import supportedComponents from './supportedComponents';
 
 export default function extendThemeWith(theme) {
-  return (parentTheme = {}) => merge({}, parentTheme, theme, components.reduce((prev, c) => ({
+  return (parentTheme = {}) => merge({}, parentTheme, theme, supportedComponents.reduce((prev, c) => ({
     ...prev,
     [c]: componentParts => props => css`
     ${parentTheme[c] ? parentTheme[c](componentParts)(props) : ''}
