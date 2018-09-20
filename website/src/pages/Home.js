139868decad8faa12c4dc8b88307b643a0bc51e2
Andrew Smith <andrew@andrew.codes>
2018-09-20 18:51:04 -0400

    feat(website): use website directory for docs/docsz site

--- website/src/pages/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Redirect } from 'react-router';
-
-export default function Home() {
-  return (
-    <Redirect from="/" to="/docs/getting-started/start-here" />
-  );
-}
