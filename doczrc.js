a5b90dd9d4ffeca9350ce6722e8c37c669394d60
Andrew Smith <andrew@andrew.codes>
2018-09-28 10:21:25 -0400

    fix(*): properly depend on prop-types
    Provide an option to remove them in production builds via the `babel-plugin-transform-react-remove-prop-types` babel plugin.

--- doczrc.js
+++ doczrc.js
@@ -22,7 +22,13 @@ export default {
                   presets: ['@babel/preset-env', '@babel/preset-react'],
                   plugins: [
                     '@babel/plugin-proposal-object-rest-spread',
-                    '@babel/plugin-syntax-dynamic-import',
+                    [
+                      'babel-plugin-transform-react-remove-prop-types',
+                      {
+                        mode: 'wrap',
+                        ignoreFilenames: ['node_modules'],
+                      },
+                    ],
                   ],
                 },
               },
