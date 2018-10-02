7716a4ca83e4ec8b414dca2c5f737702c51f4564
Andrew Smith <andrew@andrew.codes>
2018-10-02 08:23:12 -0400

    Do not show if a theme value is required; all are false as there is a provided default

--- build/docz/src/ThemeDefinitionTable.js
+++ build/docz/src/ThemeDefinitionTable.js
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { applyStaticStyles } from '@andrew-codes/verdigris-style-container';
 import { PropsTable as Table } from 'docz';
 
 const PropsTable = ({ of }) => (
-  <Table
-    of={{
-      __docgenInfo: {
-        props: Array.isArray(of) ? of[0].props : of.props,
-      },
-    }}
-  />
+  <div data-component="ThemeDefinitionTable">
+    <Table
+      of={{
+        __docgenInfo: {
+          props: Array.isArray(of) ? of[0].props : of.props,
+        },
+      }}
+    />
+  </div>
 );
 PropsTable.propTypes = {
   of: PropTypes.oneOfType(
@@ -22,4 +25,9 @@ PropsTable.defaultProps = {
     props: {},
   },
 };
-export default PropsTable;
+export default applyStaticStyles(
+  {
+    display: 'none',
+  },
+  '[data-component="ThemeDefinitionTable"] thead th:nth-of-type(3), [data-component="ThemeDefinitionTable"] tbody td:nth-of-type(3)',
+)(PropsTable);
