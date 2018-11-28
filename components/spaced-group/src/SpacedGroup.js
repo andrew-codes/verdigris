e8b10652d5ef67cb1c0868d96231c78afa52d088
Andrew Smith <andrew@andrew.codes>
2018-11-28 08:38:53 -0500

    Skeleton SpacedGroup package

---
+++ components/spaced-group/src/SpacedGroup.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const SpacedGroup = () => {
+  throw new Error("Not implemented");
+};
+SpacedGroup.propTypes = {
+};
+SpacedGroup.defaultProps = {
+};
+SpacedGroup.themeDefinition = {
+};
+SpacedGroup.defaultThemeValues = {
+};
+
+export default SpacedGroup;
