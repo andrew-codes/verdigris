6fbaf6ddb3010f758fcfa53dd00e962ea227f97b
Andrew Smith <andrew@andrew.codes>
2018-12-04 09:13:24 -0500

    Tests

---
+++ cypress/integration/components/Hidden.spec.js
@@ -0,0 +1,27 @@
+context('@andrew-codes/verdigris-hidden', () => {
+  before(() => {
+    cy.navigate('Layout', 'Hidden');
+  });
+  it('documentation page contains API section for Analytics components', () => {
+    cy.contains('h2', 'Component API')
+      .next('[data-component="PropsTable"]')
+      .should('exist');
+  });
+  it('visually hidden items are focus-able', () => {
+    cy.get('[data-test="visually-hidden"]')
+      .find('a')
+      .first()
+      .then(el => {
+        el.get(0).focus();
+      })
+      .should('have.css', 'position', 'static');
+  });
+  it.only('can be hidden from screen readers', () => {
+    cy.get('[data-test="hidden-from-screenreaders"]')
+      .find('a')
+      .first()
+      .children()
+      .first()
+      .should('have.attr', 'aria-hidden', 'true');
+  });
+});
